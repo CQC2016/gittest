54bdcb26a5678d0aad89ba606aa5ccb7915bd63d
CQC2016 <cen.qichang@byd.com>
2020-05-09 09:44:25 +0800

    code done

--- code.c
+++ code.c
@@ -1,10 +1,14 @@
 #include "stdio.h"
 
+int max(int a, int b)
+{
+	return a>b?a:b;
+}
+
+
 void main(void)
 {
 	int a = 1;
 	int b = 3;
-	max(a,b);
-<<<<<<< HEAD
-	printf("");
+	printf("The max number is : %d", max(a,b));
 }
